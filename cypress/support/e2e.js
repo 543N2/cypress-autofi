442f834759297fd30315fb33ee43049098df6e9f
Juan Saenz <543n2.one@gmail.com>
2023-06-19 00:36:12 -0500

    issue01: Muted fetch/xhr requests in log and prevented existent uncaught exception

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -18,3 +18,21 @@ import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+
+// Return false to prevent the exception from being logged
+Cypress.on('uncaught:exception', () => {
+    return false;
+  });
+
+
+// Hide fetch/XHR requests
+const app = window.top;
+if (!app.document.head.querySelector('[data-hide-command-log-request]')) {
+  const style = app.document.createElement('style');
+  style.innerHTML =
+    '.command-name-request, .command-name-xhr { display: none }';
+  style.setAttribute('data-hide-command-log-request', '');
+
+  app.document.head.appendChild(style);
+}
